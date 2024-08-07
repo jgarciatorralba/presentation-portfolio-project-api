be58ae6c6b541c9369d65c450f52d2adf2d3a708
Jorge García <jgarciatorralba@gmail.com>
2024-08-07 21:45:23 +0200

    update create project controller

--- src/UI/Controller/Projects/CreateProjectController.php
+++ src/UI/Controller/Projects/CreateProjectController.php
@@ -4,15 +4,35 @@ declare(strict_types=1);
 
 namespace App\UI\Controller\Projects;
 
+use App\Projects\Application\Command\CreateProject\CreateProjectCommand;
+use App\UI\Controller\BaseController;
+use App\UI\Request\Projects\CreateProjectRequest;
+use DateTimeImmutable;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
-final class CreateProjectController
+final class CreateProjectController extends BaseController
 {
-    public function __invoke(): Response
+    public function __invoke(CreateProjectRequest $request): Response
     {
+        $data = $request->payload();
+
+        $this->dispatch(
+            new CreateProjectCommand(
+                id: $data['id'],
+                name: $data['name'],
+                description: $data['description'] ?? null,
+                topics: $data['topics'] ?? null,
+                repository: $data['repository'],
+                homepage: $data['homepage'] ?? null,
+                archived: $data['archived'],
+                lastPushedAt: $data['lastPushedAt'],
+                createdAt: new DateTimeImmutable()
+            )
+        );
+
         return new JsonResponse([
-            'id' => 123
+            'id' => $data['id']
         ], Response::HTTP_CREATED);
     }
 }
