a6ac0882effa05715d036761bf9cceeccc8de67d
Jorge García <jgarciatorralba@gmail.com>
2024-08-09 19:39:37 +0200

    add local DateTimeZone converter

---
+++ src/Projects/Domain/Service/LocalDateTimeZoneConverter.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Projects\Domain\Service;
+
+use DateTimeImmutable;
+use DateTimeZone;
+
+final class LocalDateTimeZoneConverter
+{
+    private string $localTimeZone;
+
+    public function __construct()
+    {
+        $this->localTimeZone = date_default_timezone_get();
+    }
+
+    public function convert(DateTimeImmutable $dateTime): DateTimeImmutable
+    {
+        return $dateTime->setTimezone(new DateTimeZone($this->localTimeZone));
+    }
+}
