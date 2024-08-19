7e4070dd7f95d19ede6b431983b8d50ab126d2dc
Jorge García <jgarciatorralba@gmail.com>
2024-08-19 11:51:39 +0200

    add unit test for SoftDeleteable filter

---
+++ tests/Unit/Shared/Infrastructure/Persistence/Doctrine/Filter/SoftDeleteableFilterTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Shared\Infrastructure\Persistence\Doctrine\Filter;
+
+use App\Shared\Infrastructure\Persistence\Doctrine\Filter\SoftDeleteableFilter;
+use App\Tests\Unit\Shared\TestCase\EntityManagerMock;
+use Doctrine\ORM\Mapping\ClassMetadata;
+use PHPUnit\Framework\TestCase;
+
+final class SoftDeleteableFilterTest extends TestCase
+{
+    private ?EntityManagerMock $entityManager;
+    private ?string $tableAlias;
+
+    protected function setUp(): void
+    {
+        $this->entityManager = new EntityManagerMock();
+        $this->tableAlias = 'alias';
+    }
+
+    protected function tearDown(): void
+    {
+        $this->entityManager = null;
+        $this->tableAlias = null;
+    }
+
+    public function testItAddsFilterConstraint(): void
+    {
+        $filter = new SoftDeleteableFilter(
+            em: $this->entityManager->getMock()
+        );
+
+        $metadata = $this->createMock(ClassMetadata::class);
+
+        $result = $filter->addFilterConstraint($metadata, $this->tableAlias);
+        $this->assertEquals("{$this->tableAlias}.deleted_at IS NULL", $result);
+    }
+}
