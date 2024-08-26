55c6513015eaffbb0477d0e14ba17f78e08d96ad
Jorge García <jgarciatorralba@gmail.com>
2024-08-26 16:03:39 +0200

    add unit test for ValidationException

---
+++ tests/Unit/UI/Exception/ValidationExceptionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\UI\Exception;
+
+use App\UI\Exception\ValidationException;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class ValidationExceptionTest extends TestCase
+{
+    #[DataProvider('dataIsCreated')]
+    /**
+     * @param array<string, string> $errors
+     */
+    public function testExceptionIsCreated(ValidationException $exception, array $errors): void
+    {
+        $this->assertEquals('Invalid request data.', $exception->getMessage());
+        $this->assertEquals(Response::HTTP_BAD_REQUEST, $exception->getStatusCode());
+        $this->assertEquals($errors, $exception->getErrors());
+    }
+
+    /**
+     * @return array<string, array<ValidationException, array<string, string>>>
+     */
+    public static function dataIsCreated(): array
+    {
+        $errors = [
+            'foo' => 'bar',
+            'baz' => 'qux',
+        ];
+
+        return [
+            'errors' => [
+                new ValidationException($errors),
+                $errors
+            ],
+            'no errors' => [
+                new ValidationException(),
+                []
+            ],
+        ];
+    }
+}
