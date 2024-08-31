23ebb3bb7438c3665cc6c82f7643de384dc0e883
Jorge García <jgarciatorralba@gmail.com>
2024-09-01 00:15:34 +0200

    group common code used in tests

--- tests/Unit/UI/Controller/Projects/GetProjectsControllerTest.php
+++ tests/Unit/UI/Controller/Projects/GetProjectsControllerTest.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\UI\Controller\Projects;
 
-use App\Tests\Unit\UI\TestCase\InMemorySymfonyCommandBusMock;
-use App\Tests\Unit\UI\TestCase\InMemorySymfonyQueryBusMock;
-use App\Tests\Unit\UI\TestCase\ValidatorMock;
+use App\Shared\Domain\Bus\Command\CommandBus;
 use App\Shared\Domain\Bus\Query\Response;
+use App\Tests\Unit\UI\TestCase\InMemorySymfonyQueryBusMock;
 use App\UI\Controller\Projects\GetProjectsController;
 use App\UI\Request\Projects\GetProjectsRequest;
+use App\UI\Validation\Validator;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,45 +19,38 @@ use Symfony\Component\HttpFoundation\Response as HttpResponse;
 final class GetProjectsControllerTest extends TestCase
 {
     private ?InMemorySymfonyQueryBusMock $queryBusMock;
-    private ?InMemorySymfonyCommandBusMock $commandBusMock;
-    private ?ValidatorMock $validatorMock;
-    private ?RequestStack $requestStack;
     private ?GetProjectsController $sut;
+    private ?GetProjectsRequest $getProjectsRequest;
 
     protected function setUp(): void
     {
         $this->queryBusMock = new InMemorySymfonyQueryBusMock();
-        $this->commandBusMock = new InMemorySymfonyCommandBusMock();
-        $this->validatorMock = new ValidatorMock();
-        $this->requestStack = new RequestStack();
         $this->sut = new GetProjectsController(
             queryBus: $this->queryBusMock->getMock(),
-            commandBus: $this->commandBusMock->getMock()
+            commandBus: $this->createMock(CommandBus::class)
+        );
+
+        $requestStack = new RequestStack();
+        $requestStack->push(new Request());
+        $this->getProjectsRequest = new GetProjectsRequest(
+            validator: $this->createMock(Validator::class),
+            request: $requestStack
         );
     }
 
     protected function tearDown(): void
     {
         $this->queryBusMock = null;
-        $this->commandBusMock = null;
-        $this->validatorMock = null;
-        $this->requestStack = null;
         $this->sut = null;
+        $this->getProjectsRequest = null;
     }
 
     public function testItReturnsResponse(): void
     {
-        $this->requestStack->push(new Request());
-
-        $getProjectsRequest = new GetProjectsRequest(
-            validator: $this->validatorMock->getMock(),
-            request: $this->requestStack
-        );
-
         $this->queryBusMock
             ->willGetResult($this->createMock(Response::class));
 
-        $result = $this->sut->__invoke($getProjectsRequest);
+        $result = $this->sut->__invoke($this->getProjectsRequest);
 
         $this->assertInstanceOf(JsonResponse::class, $result);
         $this->assertEquals($result->getStatusCode(), HttpResponse::HTTP_OK);
@@ -66,18 +59,11 @@ final class GetProjectsControllerTest extends TestCase
 
     public function testItThrowsException(): void
     {
-        $this->requestStack->push(new Request());
-
-        $getProjectsRequest = new GetProjectsRequest(
-            validator: $this->validatorMock->getMock(),
-            request: $this->requestStack
-        );
-
         $this->queryBusMock
             ->willThrowException($this->createMock(\Exception::class));
 
         $this->expectException(\Exception::class);
 
-        $this->sut->__invoke($getProjectsRequest);
+        $this->sut->__invoke($this->getProjectsRequest);
     }
 }
