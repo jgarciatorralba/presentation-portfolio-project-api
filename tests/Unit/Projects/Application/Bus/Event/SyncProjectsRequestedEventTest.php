261cdd15f852c978691b0f387e71a39fed784c26
Jorge García <jgarciatorralba@gmail.com>
2024-09-23 19:55:56 +0200

    add unit test for SyncProjectsRequestedEvent

---
+++ tests/Unit/Projects/Application/Bus/Event/SyncProjectsRequestedEventTest.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Projects\Application\Bus\Event;
+
+use App\Projects\Application\Bus\Event\SyncProjectsRequestedEvent;
+use App\Shared\Application\Bus\Event\ApplicationEvent;
+use PHPUnit\Framework\TestCase;
+
+final class SyncProjectsRequestedEventTest extends TestCase
+{
+    private ?SyncProjectsRequestedEvent $syncProjectsRequestedEvent;
+
+    protected function setUp(): void
+    {
+        $this->syncProjectsRequestedEvent = new SyncProjectsRequestedEvent();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->syncProjectsRequestedEvent = null;
+    }
+
+    public function testItShouldBeAnInstanceOfApplicationEvent(): void
+    {
+        $this->assertInstanceOf(
+            ApplicationEvent::class,
+            $this->syncProjectsRequestedEvent
+        );
+    }
+
+    public function testItShouldHaveAnEventId(): void
+    {
+        $this->assertNotEmpty(
+            $this->syncProjectsRequestedEvent->eventId()
+        );
+    }
+
+    public function testItShouldHaveAnOccurredOn(): void
+    {
+        $this->assertNotEmpty(
+            $this->syncProjectsRequestedEvent->occurredOn()
+        );
+    }
+
+    public function testItShouldHaveAnEventType(): void
+    {
+        $this->assertEquals(
+            'projects.application.sync_projects_requested',
+            $this->syncProjectsRequestedEvent::eventType()
+        );
+    }
+
+    public function testItShouldHaveAnArrayRepresentation(): void
+    {
+        $arrayRepresentation = $this->syncProjectsRequestedEvent->toArray();
+
+        $this->assertIsArray($arrayRepresentation);
+        $this->assertArrayHasKey('id', $arrayRepresentation);
+        $this->assertArrayHasKey('eventType', $arrayRepresentation);
+        $this->assertArrayHasKey('occurredOn', $arrayRepresentation);
+        $this->assertArrayHasKey('attributes', $arrayRepresentation);
+    }
+}
