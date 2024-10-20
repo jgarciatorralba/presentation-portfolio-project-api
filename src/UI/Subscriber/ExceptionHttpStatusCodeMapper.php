223c81230de6d80069d14904ef20004e1122d241
Jorge García <jgarciatorralba@gmail.com>
2024-10-20 19:02:59 +0200

    add new exceptions to status code mapper

--- src/UI/Subscriber/ExceptionHttpStatusCodeMapper.php
+++ src/UI/Subscriber/ExceptionHttpStatusCodeMapper.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\UI\Subscriber;
 
+use App\Projects\Domain\Exception\InvalidProjectIdException;
+use App\Projects\Domain\Exception\InvalidProjectRepositoryUrlException;
 use App\Projects\Domain\Exception\ProjectAlreadyExistsException;
 use App\Projects\Domain\Exception\ProjectNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 final class ExceptionHttpStatusCodeMapper
 {
     private const EXCEPTIONS = [
+        InvalidProjectIdException::class => Response::HTTP_BAD_REQUEST,
+        InvalidProjectRepositoryUrlException::class => Response::HTTP_BAD_REQUEST,
         ProjectNotFoundException::class => Response::HTTP_NOT_FOUND,
         ProjectAlreadyExistsException::class => Response::HTTP_CONFLICT,
     ];
