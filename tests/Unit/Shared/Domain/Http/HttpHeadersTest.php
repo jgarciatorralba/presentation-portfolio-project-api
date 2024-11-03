f25be2e62af9574ca111acca70697c84edd45fed
Jorge García <jgarciatorralba@gmail.com>
2024-11-03 20:35:42 +0100

    complete unit tests for HttpHeaders VO

--- tests/Unit/Shared/Domain/Http/HttpHeadersTest.php
+++ tests/Unit/Shared/Domain/Http/HttpHeadersTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Unit\Shared\Domain\Http;
 
+use App\Shared\Domain\Http\HttpHeader;
 use App\Shared\Domain\Http\HttpHeaders;
 use App\Tests\Builder\Shared\Domain\Http\HttpHeaderBuilder;
 use App\Tests\Builder\Shared\Domain\Http\HttpHeadersBuilder;
@@ -19,7 +20,7 @@ final class HttpHeadersTest extends TestCase
         $this->assertNotEmpty($headers->all());
     }
 
-    public function testTheyAreMergedWhenHeaderNameIsTheSame(): void
+    public function testTheyAreMergedWhenNameIsTheSame(): void
     {
         $firstHeader = HttpHeaderBuilder::any()
             ->withName('a-valid-header')
@@ -32,9 +33,55 @@ final class HttpHeadersTest extends TestCase
         $headers = new HttpHeaders($firstHeader, $secondHeader);
 
         $this->assertCount(1, $headers->all());
-        $this->assertSame(
+        $this->assertEquals(
             count($firstHeader->values()) + count($secondHeader->values()),
             count($headers->get($firstHeader->name())->values())
+        );
+    }
+
+    public function testHeadersCanBeRetrieved(): void
+    {
+        $headers = HttpHeadersBuilder::any()->build();
+
+        $this->assertNotEmpty($headers->all());
+        $this->assertContainsOnlyInstancesOf(HttpHeader::class, $headers->all());
+        $this->assertCount(count($headers->all()), $headers->all());
+    }
+
+    public function testHeadersHaveKey(): void
+    {
+        $expected = HttpHeaderBuilder::any()->build();
+
+        $headers = new HttpHeaders($expected);
+
+        $this->assertTrue($headers->has($expected->name()));
+    }
+
+    public function testHeaderCanBeRetrievedByKey(): void
+    {
+        $expected = HttpHeaderBuilder::any()->build();
+
+        $headers = new HttpHeaders($expected);
+
+        $this->assertEquals($expected, $headers->get($expected->name()));
+    }
+
+    public function testTheyAreMappable(): void
+    {
+        $headers = HttpHeadersBuilder::any()->build();
+
+        $mappedHeaders = $headers->toArray();
+
+        $this->assertIsArray($mappedHeaders);
+        $this->assertNotEmpty($mappedHeaders);
+        $this->assertEquals(
+            array_keys($mappedHeaders),
+            array_map(fn (HttpHeader $header): string => $header->name(), $headers->all())
+        );
+        $this->assertEquals(
+            array_values($mappedHeaders),
+            /** @return string[] */
+            array_map(fn (HttpHeader $header): array => $header->values(), $headers->all())
         );
     }
 }
