8f604a846a9e9cab354482fa0a576b14dd6b700d
Jorge García <jgarciatorralba@gmail.com>
2024-11-09 18:08:07 +0100

    complete unit tests for TemporaryFileStream

--- tests/Unit/Shared/Infrastructure/Http/TemporaryFileStreamTest.php
+++ tests/Unit/Shared/Infrastructure/Http/TemporaryFileStreamTest.php
@@ -70,8 +70,129 @@ final class TemporaryFileStreamTest extends TestCase
         $charsToRead = FakeValueGenerator::integer(
             max: strlen($content)
         );
-        $stream->read($charsToRead);
+        $result = $stream->read($charsToRead);
 
-        $this->assertEquals($charsToRead, $stream->tell());
+        $this->assertEquals(substr($content, 0, $charsToRead), $result);
+    }
+
+    public function testItChecksIfTheStreamIsAtTheEnd(): void
+    {
+        $content = FakeValueGenerator::text();
+        $stream = new TemporaryFileStream($content);
+
+        $stream->getContents();
+
+        $this->assertTrue($stream->eof());
+    }
+
+    public function testItChecksIfTheStreamIsSeekable(): void
+    {
+        $stream = new TemporaryFileStream();
+
+        $this->assertTrue($stream->isSeekable());
+    }
+
+    public function testItSeeksToAPositionInTheStream(): void
+    {
+        $content = FakeValueGenerator::text();
+        $stream = new TemporaryFileStream($content);
+
+        $position = FakeValueGenerator::integer(
+            max: strlen($content)
+        );
+        $result = $stream->seek($position);
+
+        $this->assertEquals($position, $stream->tell());
+        $this->assertEmpty($result);
+    }
+
+    public function testItRewindsTheStream(): void
+    {
+        $content = FakeValueGenerator::text();
+        $stream = new TemporaryFileStream($content);
+
+        $position = FakeValueGenerator::integer(
+            max: strlen($content)
+        );
+
+        $stream->read($position);
+        $stream->rewind();
+
+        $this->assertEquals(0, $stream->tell());
+    }
+
+    public function testItChecksIfTheStreamIsWritable(): void
+    {
+        $stream = new TemporaryFileStream();
+
+        $this->assertTrue($stream->isWritable());
+    }
+
+    public function testItWritesToTheStream(): void
+    {
+        $stream = new TemporaryFileStream();
+        $content = FakeValueGenerator::text();
+
+        $stream->write($content);
+
+        $this->assertEquals($content, (string) $stream);
+    }
+
+    public function testItChecksIfTheStreamIsReadable(): void
+    {
+        $stream = new TemporaryFileStream();
+
+        $this->assertTrue($stream->isReadable());
+    }
+
+    public function testItReadsFromTheStream(): void
+    {
+        $content = FakeValueGenerator::text();
+        $stream = new TemporaryFileStream($content);
+
+        $charsToRead = FakeValueGenerator::integer(
+            max: strlen($content)
+        );
+        $result = $stream->read($charsToRead);
+
+        $this->assertEquals(substr($content, 0, $charsToRead), $result);
+    }
+
+    public function testItGetsTheMetadataOfTheStream(): void
+    {
+        $stream = new TemporaryFileStream();
+
+        $metadata = $stream->getMetadata();
+
+        $this->assertIsArray($metadata);
+
+        $keys = ['wrapper_type', 'stream_type', 'mode', 'unread_bytes', 'uri', 'seekable'];
+        foreach ($keys as $key) {
+            $this->assertArrayHasKey($key, $metadata);
+        }
+    }
+
+    public function testItGetsTheMetadataOfTheStreamByKey(): void
+    {
+        $stream = new TemporaryFileStream();
+
+        $keys = ['wrapper_type', 'stream_type', 'mode', 'unread_bytes', 'uri', 'seekable'];
+        foreach ($keys as $key) {
+            $metadata = $stream->getMetadata($key);
+
+            $this->assertNotNull($metadata);
+        }
+
+        $this->assertNull($stream->getMetadata('non_existent_key'));
+    }
+
+    public function testItGetsTheContentsOfTheStream(): void
+    {
+        $content = FakeValueGenerator::text();
+        $stream = new TemporaryFileStream($content);
+
+        $result = $stream->getContents();
+
+        $this->assertEquals($content, $result);
     }
 }
