33c047f5271a6ad8870c7ff196499feddbcf846d
Jorge García <jgarciatorralba@gmail.com>
2024-12-01 19:41:40 +0100

    create canConnectToDatabase trait

--- tests/Feature/FeatureTestCase.php
+++ tests/Feature/FeatureTestCase.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 namespace App\Tests\Feature;
 
-use Doctrine\DBAL\Exception as DBALException;
-use Doctrine\DBAL\Platforms\AbstractPlatform;
-use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use App\Tests\Feature\Trait\CanConnectToDatabase;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\DependencyInjection\Exception\ServiceCircularReferenceException;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
@@ -16,7 +13,8 @@ use Symfony\Component\HttpKernel\HttpKernelBrowser;
 
 abstract class FeatureTestCase extends WebTestCase
 {
-    private ?EntityManagerInterface $entityManager = null;
+    use CanConnectToDatabase;
+
     protected ?HttpKernelBrowser $client = null;
 
     /**
@@ -40,100 +38,5 @@ abstract class FeatureTestCase extends WebTestCase
         $this->entityManager = null;
 
         parent::tearDown();
-    }
-
-    /**
-     * @template T of object
-     * @param class-string<T> $className
-     * @param array<string, mixed> $criteria
-     */
-    protected function findOneBy(string $className, array $criteria): ?object
-    {
-        return $this->repository($className)->findOneBy($criteria);
-    }
-
-    protected function persist(object ...$entities): void
-    {
-        foreach ($entities as $entity) {
-            $this->entityManager->persist($entity);
-        }
-
-        $this->entityManager->flush();
-    }
-
-    protected function remove(object $entity): void
-    {
-        $this->entityManager->remove($entity);
-        $this->entityManager->flush();
-    }
-
-    /**
-     * @template T of object
-     * @param class-string<T> $className
-     * @return EntityRepository<T>
-     */
-    protected function repository(string $className): EntityRepository
-    {
-        return $this->entityManager->getRepository($className);
-    }
-
-    /** @throws DBALException */
-    protected function clearDatabase(): void
-    {
-        $connection = $this->entityManager->getConnection();
-        foreach ($this->tables() as $table) {
-            $connection->executeQuery(
-                sprintf('TRUNCATE "%s" CASCADE;', $table)
-            );
-        }
-
-        $this->entityManager->clear();
-    }
-
-    /**
-     * @throws DBALException
-     *
-     * @return string[]
-     */
-    private function tables(): array
-    {
-        $notMappedSuperClassNames = array_filter(
-            $this->entityManager
-                ->getConfiguration()
-                ->getMetadataDriverImpl()
-                ->getAllClassNames(),
-            fn (string $class): bool => false ===
-                $this->entityManager
-                    ->getClassMetadata($class)
-                    ->isMappedSuperclass
-        );
-
-        $primaryTableNames = array_map(
-            fn (string $class): string =>
-                $this->entityManager
-                    ->getClassMetadata($class)
-                    ->getTableName(),
-            $notMappedSuperClassNames
-        );
-
-        $existingTables = $this->schemaManager()
-            ->listTableNames();
-
-        return array_filter(
-            $primaryTableNames,
-            fn (string $table): bool => in_array($table, $existingTables)
-        );
-    }
-
-    /**
-     * @throws DBALException
-     *
-     * @return AbstractSchemaManager<AbstractPlatform>
-     */
-    private function schemaManager(): AbstractSchemaManager
-    {
-        return $this->entityManager
-            ->getConnection()
-            ->createSchemaManager();
     }
 }
