3d2578d342254dcde725b3729256ef032aa9312d
Jorge García <jgarciatorralba@gmail.com>
2024-12-06 11:38:33 +0100

    fix code sniffer issues

--- tests/Unit/Shared/Infrastructure/Http/Symfony/SymfonyHttpClientTest.php
+++ tests/Unit/Shared/Infrastructure/Http/Symfony/SymfonyHttpClientTest.php
@@ -57,8 +57,8 @@ final class SymfonyHttpClientTest extends TestCase
         ];
 
         $response = $this->httpClient->fetch('/posts', $httpOptions);
-		$content = $response->getBody()->getContents();
-		$decodedContent = json_decode($content, true);
+        $content = $response->getBody()->getContents();
+        $decodedContent = json_decode($content, true);
 
         $this->assertInstanceOf(HttpResponse::class, $response);
         $this->assertSame(
@@ -76,10 +76,10 @@ final class SymfonyHttpClientTest extends TestCase
         $this->assertInstanceOf(TemporaryFileStream::class, $response->getBody());
         $this->assertNotEmpty($content);
         $this->assertIsArray($decodedContent);
-		$this->assertEquals(
-			json_decode(self::RESPONSE_BODY, true),
-			$decodedContent['content']
-		);
-		$this->assertNull($decodedContent['error']);
+        $this->assertEquals(
+            json_decode(self::RESPONSE_BODY, true),
+            $decodedContent['content']
+        );
+        $this->assertNull($decodedContent['error']);
     }
 }
