0db44293ff091a50985b204155d1c331d28200f3
Jorge García <jgarciatorralba@gmail.com>
2024-12-08 19:12:39 +0100

    add unit tests for RequestExternalProjects service

---
+++ tests/Unit/Projects/Domain/Service/RequestExternalProjectsTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Projects\Domain\Service;
+
+use App\Projects\Domain\Project;
+use App\Projects\Domain\Service\RequestExternalProjects;
+use App\Tests\Builder\Projects\Domain\ProjectBuilder;
+use App\Tests\Unit\Projects\TestCase\ExternalProjectRetrieverMock;
+use PHPUnit\Framework\TestCase;
+
+final class RequestExternalProjectsTest extends TestCase
+{
+	/** @var Project[] $projects */
+	private ?array $projects;
+	private ?ExternalProjectRetrieverMock $externalProjectRetrieverMock;
+	private ?RequestExternalProjects $sut;
+
+	protected function setUp(): void
+	{
+		$this->projects = ProjectBuilder::buildMany();
+		$this->externalProjectRetrieverMock = new ExternalProjectRetrieverMock();
+		$this->sut = new RequestExternalProjects(
+			externalProjectRetriever: $this->externalProjectRetrieverMock->getMock()
+		);
+	}
+
+	protected function tearDown(): void
+	{
+		$this->projects = null;
+		$this->externalProjectRetrieverMock = null;
+		$this->sut = null;
+	}
+
+	public function testItRequestsExternalProjects(): void
+	{
+		$this->externalProjectRetrieverMock
+			->shouldRetrieveProjects(...$this->projects);
+
+		$result = $this->sut->__invoke();
+
+		$mappedProjects = array_reduce(
+			$this->projects,
+			static function (array $carry, Project $project): array {
+				$carry[$project->id()->value()] = $project;
+				return $carry;
+			},
+			[]
+		);
+
+		$this->assertEquals(
+			$mappedProjects,
+			$result
+		);
+	}
+}
---
+++ tests/Unit/Projects/TestCase/ExternalProjectRetrieverMock.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Projects\TestCase;
+
+use App\Projects\Domain\Contract\ExternalProjectRetriever;
+use App\Projects\Domain\Project;
+use App\Tests\Unit\Shared\Infrastructure\Testing\AbstractMock;
+use PHPUnit\Framework\InvalidArgumentException;
+use PHPUnit\Framework\MockObject\IncompatibleReturnValueException;
+use PHPUnit\Framework\MockObject\MethodCannotBeConfiguredException;
+use PHPUnit\Framework\MockObject\MethodNameAlreadyConfiguredException;
+
+final class ExternalProjectRetrieverMock extends AbstractMock
+{
+	protected function getClassName(): string
+	{
+		return ExternalProjectRetriever::class;
+	}
+
+	/**
+	 * @throws IncompatibleReturnValueException
+	 * @throws InvalidArgumentException
+	 * @throws MethodCannotBeConfiguredException
+	 * @throws MethodNameAlreadyConfiguredException
+	 */
+	public function shouldRetrieveProjects(Project ...$projects): void
+	{
+		$this->mock
+			->expects($this->once())
+			->method('retrieve')
+			->willReturn($projects);
+	}
+}
