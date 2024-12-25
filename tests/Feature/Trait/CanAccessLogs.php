3fe1f81edfede241b46806a80b994272eb2b93cb
Jorge García <jgarciatorralba@gmail.com>
2024-12-25 10:36:10 +0100

    update method visibility in trait

--- tests/Feature/Trait/CanAccessLogs.php
+++ tests/Feature/Trait/CanAccessLogs.php
@@ -8,7 +8,7 @@ trait CanAccessLogs
 {
     private const string LOGS_PATH = '/var/log/test/project.log';
 
-    protected function clearLogs(): void
+    private function clearLogs(): void
     {
         file_put_contents(
             dirname(__DIR__, 3) . self::LOGS_PATH,
@@ -16,7 +16,7 @@ trait CanAccessLogs
         );
     }
 
-    protected function assertLogContains(string $message): void
+    private function assertLogContains(string $message): void
     {
         $logs = file_get_contents(
             dirname(__DIR__, 3) . self::LOGS_PATH
