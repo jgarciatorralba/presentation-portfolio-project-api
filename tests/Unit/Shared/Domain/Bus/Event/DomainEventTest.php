ea14f76c8e78690d8b5c5a81b7213734e1db1d49
Jorge García <jgarciatorralba@gmail.com>
2025-04-20 10:48:33 +0200

    Add unit test for DomainEvent

---
+++ tests/Unit/Shared/Domain/Bus/Event/DomainEventTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Shared\Domain\Bus\Event;
+
+use PHPUnit\Framework\TestCase;
+use App\Shared\Domain\Bus\Event\DomainEvent;
+
+final class DomainEventTest extends TestCase
+{
+	public function testItIsMappable(): void
+	{
+		$testAggregateId = 'abc123';
+
+		$event = new class($testAggregateId) extends DomainEvent {
+			public function __construct(
+				private string $aggregateId,
+			) {
+				parent::__construct($aggregateId);
+			}
+
+			public static function eventType(): string
+			{
+				return 'test.event';
+			}
+		};
+
+		$this->assertEquals([
+			'id' => $event->eventId(),
+			'eventType' => 'test.event',
+			'occurredOn' => $event->occurredOn(),
+			'attributes' => [
+				'aggregateId' => $testAggregateId,
+			],
+		], $event->toArray());
+	}
+}
