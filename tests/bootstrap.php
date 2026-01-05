7ea67fc99b77df5c7b577bd32d8b3354f926f50f
Jorge García <jgarciatorralba@gmail.com>
2026-01-05 19:20:20 +0100

    update phpunit symfony recipe

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\ErrorHandler;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
-set_exception_handler([new ErrorHandler(), 'handleException']);
-
-new Dotenv()->bootEnv(dirname(__DIR__) . '/.env');
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    new Dotenv()->bootEnv(dirname(__DIR__).'/.env');
+}
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
